ab24d9ea8740279b133b8649c68dfa116f05ab93
MarkusS <markusscoding@outlook.com>
2020-11-04 12:53:49 +0100

    first commit

---
+++ precache-manifest.36461b1fbdee4dc66bb7bbdd8be0a4df.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb7247f610b6262b1c69e750accd9c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4c1d71544e0216da618",
+    "url": "/static/css/main.c741440c.chunk.css"
+  },
+  {
+    "revision": "1d894998148c60c0e19c",
+    "url": "/static/js/2.6b24b256.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.6b24b256.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4c1d71544e0216da618",
+    "url": "/static/js/main.6b555fce.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "6a62d2b8f0d02cc7e262738f0fe759af",
+    "url": "/static/media/Bootstrap.6a62d2b8.png"
+  },
+  {
+    "revision": "8ba56168e2a6602d09186033da41b398",
+    "url": "/static/media/Laptop1.8ba56168.jpeg"
+  },
+  {
+    "revision": "b9bbb1d722d3ef6ebf7d573703b5925e",
+    "url": "/static/media/Python.b9bbb1d7.png"
+  },
+  {
+    "revision": "a6185c417cb861a10e6afc53b3ef45e1",
+    "url": "/static/media/React.a6185c41.png"
+  },
+  {
+    "revision": "b7526df2f32a7a3c424aed83be682096",
+    "url": "/static/media/selfie.b7526df2.jpg"
+  }
+]);
